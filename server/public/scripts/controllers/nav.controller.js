28278a4933398ea7d175f4db8b55f1256bbde008
Ben Siegel <bendrumin@Bens-MacBook-Pro-2.local>
2017-08-24 16:44:00 -0500

    BS: Creating a teachers page, adding the form and attempting am modal.

---
+++ server/public/scripts/controllers/nav.controller.js
@@ -0,0 +1,6 @@
+myApp.controller('NavController', function(UserService) {
+  console.log('NavController created');
+  var vm = this;
+  vm.userService = UserService;
+  vm.userObject = UserService.userObject;
+});
