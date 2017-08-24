5310e4ec149fe0868401aea1f0ab46b0128af1c4
Ben Siegel <bendrumin@Bens-MacBook-Pro-2.local>
2017-08-24 11:07:04 -0500

    BS: Pushing changes to master

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
 
 /// Routes ///
 myApp.config(function($routeProvider, $locationProvider) {
