2b6c0b4a06db98a1d2becc2f6a0062d0e59c5b7c
Alessandro Leyva <josealeyva87@gmail.com>
2017-08-28 14:06:00 -0500

    Students route

--- server/routes/users.router.js
+++ server/routes/users.router.js
@@ -5,7 +5,13 @@ var pool = require('../modules/pool.js');
 var encryptLib = require('../modules/encryption');
 var pg = require('pg');
 
-//NOTE get users
+// roles:
+// 1 = admin
+// 2 = store manager
+// 3 = teacher
+// 4 = student
+
+// get users
 router.get('/', function(req, res){
   // errorConnecting is bool, db is what we query against,
   // done is a function that we call when we're done
@@ -16,10 +22,10 @@ router.get('/', function(req, res){
     } else {
       // We connected to the database!!!
       // Now we're going to GET things from the db
-      var queryText = SELECT '"users"."id", "users"."name",', +
-      '  "users"."username", "users"."studentId", "users"."pic",', +
-      '  "users"."pts", "users"."lifetimePts", "users"."email"', +
-      'FROM "users" ORDER BY "role", "username" ASC;';
+      var queryText = "SELECT userid,username,", +
+          "users.username, users.studentId, users.pic,", +
+          "users.pts, users.lifetimePts, users.email,", +
+          "FROM users ORDER BY role, username ASC;"
       // errorMakingQuery is a bool, result is an object
       db.query(queryText, function(errorMakingQuery, result){
         done();
@@ -40,7 +46,31 @@ router.get('/', function(req, res){
 //get list of students
 router.get('/students', function(req, res) {
   console.log('users router get /students');
-  res.sendStatus(200);
+  // errorConnecting is bool, db is what we query against,
+  // done is a function that we call when we're done
+  pool.connect(function(errorConnectingToDatabase, db, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to the database.');
+      res.sendStatus(500);
+    } else {
+      // We connected to the database!!!
+      // Now we're going to GET things from the db
+      var queryText = "SELECT * FROM users WHERE role = 4";
+      // errorMakingQuery is a bool, result is an object
+      db.query(queryText, function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Attempted to query with', queryText);
+          console.log('Error making query');
+          res.sendStatus(500);
+        } else {
+          console.log(result.rows);
+          // Send back the results
+          res.send(result.rows);
+        }
+      }); // end query
+    } // end if
+  }); // end pool
 });
 
 //sell item to student
