362f014555876fffa868bb205618c2ec60a823ee
Alessandro Leyva <josealeyva87@gmail.com>
2017-08-29 16:21:31 -0500

    Challenges service working

--- server/public/scripts/controllers/teacherChallenges.controller.js
+++ server/public/scripts/controllers/teacherChallenges.controller.js
@@ -1,49 +1,10 @@
-myApp.controller('TeacherChallengesController', function(UserService, $http) {
+myApp.controller('TeacherChallengesController', function(UserService, $http, ChallengesService) {
   console.log('TeacherChallengesController created');
   var tcc = this;
   tcc.userService = UserService;
-  tcc.challenges = [];
-  tcc.rewards = [];
   tcc.newChallenge = {};
-
-
-  tcc.getChallenges = function() {
-    console.log('get challenges');
-    $http.get('/challenges').then(function(response) {
-      console.log(response);
-      tcc.challenges = response.data;
-    });
-  }; // end getChallenges
-
-
-  tcc.addChallenge = function(newChallenge) {
-    console.log(newChallenge);
-    $http.post('/challenges', newChallenge).then(function(response) {
-      console.log(response);
-      tcc.getChallenges();
-    });
-  }; // end addChallenge
-
-
-  tcc.updateChallenge = function(challenge) {
-    console.log('update challenge');
-    console.log(challenge);
-    $http.put('/challenges', challenge).then(function(response) {
-      console.log(response);
-      tcc.getChallenges();
-    });
-  }; // end updateChallenge
-
-
-  tcc.deleteChallenge = function(challenge) {
-    console.log('deleteChallenge');
-    console.log(challenge);
-    $http.delete('/challenge/' + challenge.id).then(function(response) {
-      console.log(response);
-      tcc.getChallenges();
-    });
-  }; // end deleteChallenge
-
-  tcc.getChallenges();
-
+  tcc.challengesService = ChallengesService;
+  tcc.challenges = ChallengesService.challenges;
+  tcc.challengesService.getChallenges();
+  console.log(tcc.challenges);
 }); // end TeacherChallengesController
