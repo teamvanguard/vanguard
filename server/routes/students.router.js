9a5bcd01bcc6838dc39520ee455767a66ae52dec
Alessandro Leyva <josealeyva87@gmail.com>
2017-08-31 09:22:12 -0500

    Post route added

--- server/routes/students.router.js
+++ server/routes/students.router.js
@@ -54,6 +54,39 @@ router.get('/challenges/:studentId', function(req, res) {
 }); // end get /challenges/:studentId
 
 
+//edit a user role
+router.post('/challenges/addStudent', function(req, res) {
+  console.log('students router post add student');
+  console.log(req.body);
+  //only admins
+  if(req.isAuthenticated()){
+    pool.connect(function(errorConnectingToDatabase, db, done){
+      if(errorConnectingToDatabase) {
+        console.log('Error connecting to the database.');
+        res.sendStatus(500);
+      } else {
+        // We connected to the database!!!
+        // Now we're going to GET things from the db
+        var queryText = ;
+        // errorMakingQuery is a bool, result is an object
+        db.query(queryText, [req.body.email, req.body.role], function(errorMakingQuery, result){
+          done();
+          if(errorMakingQuery) {
+            console.log('Attempted to query with', queryText);
+            console.log('Error making query');
+            res.sendStatus(500);
+          } else {
+            // console.log(result.rows);
+            // Send back the results
+            res.send(result.rows);
+          }
+        }); // end query
+      } // end if
+    }); // end pool
+  } else {res.sendStatus(401);} //not authorized
+});
+
+
 
 
 module.exports = router;
