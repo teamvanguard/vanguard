baf8605809b3a5d288049f492641ac037a0fcf06
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-09-01 09:11:50 -0500

    admin view users complete

---
+++ server/public/scripts/controllers/adminViewUsers.controller.js
@@ -0,0 +1,53 @@
+myApp.controller('AdminViewUsersController', function(UserService, $http, UsersService) {
+  console.log('AdminViewUsersController created');
+  var avuc = this;
+  avuc.userService = UserService;
+  avuc.usersService = UsersService
+  avuc.newUser = {};
+  // avuc.newUser.role = 2;
+  avuc.getUsers = function(){
+    avuc.users = []
+    console.log('Getting Students');
+    $http.get('/users').then(function(response){
+      console.log(response.data);
+      var users = response.data
+      console.log(users);
+      for (var i = 0; i < users.length; i++) {
+        var user = users[i];
+        console.log(user.role);
+        if (user.role == 1) {
+          user.role = 'Admin';
+        } else if (user.role == 2) {
+          user.role = 'Store Manager';
+        } else if (user.role == 3) {
+          user.role = 'Teacher';
+        } else if (user.role == 4) {
+          user.role = 'Student';
+        }
+        avuc.users.push(user);
+      }
+      console.log(avuc.users);
+    });
+  };
+  avuc.getUsers();
+  //send object with email and role properties
+  // avuc.addUser = function(newUser) {
+  //   console.log(newUser);
+  //   $http.post('/users', newUser).then(function(response) {
+  //     console.log(response);
+  //   });
+  // };
+  // //send object with email and role properties
+  // avuc.editRole = function(user) {
+  //   console.log(user);
+  //   $http.put('/users', user).then(function(response) {
+  //     console.log(response);
+  //   });
+  // };
+  // avuc.deleteUser = function(user){
+  //   console.log(user);
+  //   $http.delete('/users/' + user.id).then(function(response){
+  //     console.log(response);
+  //   });
+  // };
+});
