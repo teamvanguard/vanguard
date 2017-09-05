9f8cf0a368526470c550b4a6a54f5aa3588fd779
Antonio Rice <ricex407@gmail.com>
2017-09-05 10:41:26 -0500

    picture error fixed, default image set

--- server/public/scripts/filters/url.filter.js
+++ server/public/scripts/filters/url.filter.js
@@ -1,7 +1,12 @@
 myApp.filter('resize', function(){
   return function(url){
     //removing the last 6 characters of the url ' ?sz=50 '
-    url = url.substr(0, url.length-6);
-    return url;
+    if(url){
+      url = url.substr(0, url.length-6);
+      return url;
+    }else{
+      url ='/views/img/highschool.jpg';
+      return url;
+    }
   };
 })
