3aba249b947a48be121c68bf15c9bff324b61a3e
Ben Siegel <bendrumin@Bens-MacBook-Pro-2.local>
2017-09-11 13:53:13 -0500

    BS: Updating some minor hotfixes

--- database.setup/challenges.sql
+++ database.setup/challenges.sql
@@ -1,8 +1,8 @@
 INSERT INTO challenges(challenge_name,description,start_date,end_date,pts_value,teacher_id)
 VALUES
-('Lead your group','Take the leadership role while planning your presentation.','2017-09-11T05:00:00.000Z','2017-09-15T05:00:00.000Z',50,1),
+('Lead your group','Take the leadership role while planning your presentation.','2017-09-11T05:00:00.000Z','2017-09-15T05:00:00.000Z',50,51),
 ('Help others','Show up to school 30 minutes early 2 days this week and be a tutor for my before school math help','2017-09-18T05:00:00.000Z','2017-09-22T05:00:00.000Z',100,20),
-('Promptness','Get to class on time everyday this week','2017-09-11T05:00:00.000Z','2017-09-15T05:00:00.000Z',10,1),
+('Promptness','Get to class on time everyday this week','2017-09-11T05:00:00.000Z','2017-09-15T05:00:00.000Z',10,51),
 ('Volunteer','Attend the school organized Feed My Starving Children event','2017-09-26T05:00:00.000Z','2017-09-26T05:00:00.000Z',70,15),
 ('Blood, sweat, and tears','Participate in the schools blood drive','2017-10-04T05:00:00.000Z','2017-10-04T05:00:00.000Z',60,21),
 ('Meet someone','Sit with someone that you have never met before at lunch. Tell me who they are and 5 things you learned about them','2017-10-09T05:00:00.000Z','2017-10-13T05:00:00.000Z',40,1),
